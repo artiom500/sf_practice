6a0d0fadd7fbe61c998cb28481db68a2d2d50034
Artiom <artiom500@mail.ru>
2022-07-19 19:09:20 +0300

    Vagrantfile  Django  psycopg2

---
+++ 6/script.sh
@@ -0,0 +1,11 @@
+apt-get update
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+  sudo apt install python3.9 python3-pip -y
+    sudo apt install libpq-dev python3-dev -y
+  sudo apt install python3-psycopg2 -y	
+  sudo pip3 install Django
+ sudo mkdir -p /home/vagrant/data
+ sudo chown -R vagrant:vagrant /home/vagrant/data/
+	
